424e2bbd789772cdafb300e0279a84d454ef443c
anjalirprasad1631999-collab <anjalirprasad1631999@gmail.com>
2026-06-07 05:08:39 +0530

    Update ex_run_VF_PSO.m

--- ex_run_VF_PSO.m
+++ ex_run_VF_PSO.m
@@ -4,9 +4,7 @@
 
 format long g
 
-%%=============================================================
-% Problem Definition
-%%=============================================================
+
 
 dim = 147;
 
@@ -16,15 +14,11 @@ maxIter = 30;
 lb = zeros(1, dim);
 ub = 6 * ones(1, dim);
 
-%%=============================================================
-% Run VF-PSO
-%%=============================================================
+
 
 [best_solution, best_fitness, fitness_history] = VFPSO(@myFuzzyObjective, dim, numParticles, maxIter, lb, ub);
 
-%%=============================================================
-% Display Results
-%%=============================================================
+
 
 disp('====================================================');
 disp('Best Fitness Achieved (Minimum ITAE):');
@@ -34,9 +28,7 @@ disp('====================================================');
 disp('Best VF-PSO Solution:');
 disp(best_solution);
 
-%%=============================================================
-% Plot Fitness History
-%%=============================================================
+
 
 figure;
 
@@ -48,20 +40,9 @@ title('Fitness Value vs Iteration - VF-PSO');
 
 grid on;
 
-%%=============================================================
-% Save Results
-%%=============================================================
+
 
 results_filename = 'vfpso_results.mat';
 
 save(results_filename, 'best_solution', 'best_fitness', 'fitness_history');
 
-%%=============================================================
-% Save Readable Text Files
-%%=============================================================
-
-writematrix(best_solution,'vfpso_best_solution.txt');
-
-writematrix(best_fitness, 'vfpso_best_fitness.txt');
-
-writematrix(fitness_history, 'vfpso_fitness_history.txt');
